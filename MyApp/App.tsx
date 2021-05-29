f17a14d05db2678765ecb69f04da7a12a4251021
syafuteki <syafuteki@gmail.com>
2021-05-29 18:02:20 +0900

    進捗

---
+++ MyApp/App.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Text, View, TextInput, StyleSheet } from 'react-native';
+
+// ここから　props
+interface Props {
+    color: string;
+    children: ReactNode;
+    label: string;
+}
+
+function MyComponent(props: Props) {
+    const { color, children, label } = props;
+
+    return (
+        <View>
+            <Text style={{ color }}>{label}</Text>
+            {children}
+        </View>
+    )
+}
+// ここまで　props
+
+// ここから　ref
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    input: {
+        width: 160,
+        height: 32,
+        borderWidth: 1,
+        padding: 4,
+    }
+})
+// ここまで　ref
+
+export default function App() {
+    const ref = React.useRef<TextInput>(null);
+
+    return (
+        <View style={styles.container}>
+            <Text>Hello</Text>
+            <MyComponent color="red" label="new">
+                <Text>GoodBye</Text>
+            </MyComponent>
+
+            <TextInput ref={ref} style={styles.input} />
+            <TextInput  style={styles.input} />
+
+            <Text
+                onPress={() => {
+                    ref?.current?.focus();
+                }}>
+                focus
+                </Text>
+        </View>
+    )
+}
